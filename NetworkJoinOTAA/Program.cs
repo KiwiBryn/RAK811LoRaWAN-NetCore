89119d02b460cff12495f0159d682299147919ee
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-09-22 16:53:49 +1200

    Fixed up rhe namespace

--- NetworkJoinOTAA/Program.cs
+++ NetworkJoinOTAA/Program.cs
@@ -14,7 +14,7 @@
 // limitations under the License.
 //
 //---------------------------------------------------------------------------------
-namespace devMobile.IoT.NetCore.Rak811.NetworkJoinOTAA
+namespace devMobile.IoT.LoRaWAN.NetCore.Rak811
 {
 	using System;
 	using System.Diagnostics;
